7fac78e91887a4df7cd0e2c74e64dbdfe816dc3f
jfederico <jesus@123it.ca>
2012-07-04 10:35:04 -0400

    Restored language file format Signed-off-by: jfederico <jesus@123it.ca>

--- lang/en/recordingsbn.php
+++ lang/en/recordingsbn.php
@@ -5,8 +5,6 @@
  * Authors:
  *    Jesus Federico  (jesus [at] blindsidenetworks [dt] com)
  *    
- * Translation files available at 
- *     http://www.getlocalization.com/bigbluebutton_moodle2x
  *
  * @package   mod_bigbluebutton
  * @copyright 2011-2012 Blindside Networks Inc.
@@ -14,18 +12,16 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$string = Array(
-        'modulename' => 'RecordingsBN',
-        'modulenameplural' => 'RecordingsBN',
-        'modulename_help' => 'Use the recordingsbn module as a resource of the course in order to have access to the playback recordings related to it.',
-        'recordingsbnname' => 'Recordings name',
-        'recordingsbnname_help' => 'RecordingsBN provides a list of playback recordings in a BigBlueButton Server providing direct access to them.',
-        'recordingsbn' => 'RecordingsBN',
-        'pluginadministration' => 'recordingsbn administration',
-        'pluginname' => 'RecordingsBN',
-        'recordingsbn:view' => 'View recordings',
-        'view_noguests' => 'The RecordingsBN module is not open to guests',
-        );
+$string['modulename'] = 'RecordingsBN';
+$string['modulenameplural'] = 'RecordingsBN';
+$string['modulename_help'] = 'Use the recordingsbn module as a resource of the course in order to have access to the playback recordings related to it.';
+$string['recordingsbnname'] = 'Recordings name';
+$string['recordingsbnname_help'] = 'RecordingsBN provides a list of playback recordings in a BigBlueButton Server providing direct access to them.';
+$string['recordingsbn'] = 'RecordingsBN';
+$string['pluginadministration'] = 'recordingsbn administration';
+$string['pluginname'] = 'RecordingsBN';
+$string['recordingsbn:view'] = 'View recordings';
+$string['view_noguests'] = 'The RecordingsBN module is not open to guests';
 
 
 ?>
