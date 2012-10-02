a82f845653979bfe9291151dd7a7e55454891314
jfederico <jesus@123it.ca>
2012-10-02 12:29:25 -0400

    v1.0.8: Updated version to sync with bigbluebuttonbn Signed-off-by: jfederico <jesus@123it.ca>

--- version.php
+++ version.php
@@ -12,7 +12,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2012082300;
+$module->version   = 2012100100;
 $module->requires  = 2010112400;
 $module->cron      = 0;
 $module->component = 'mod_recordingsbn';
