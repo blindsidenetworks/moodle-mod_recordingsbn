a95e84c0273c94f3384ccf246f6475bb7541ffde
jfederico <jesus@123it.ca>
2013-02-28 16:46:03 -0500

    v1.0.9b2: Updated version tag

--- version.php
+++ version.php
@@ -6,18 +6,18 @@
  *    Jesus Federico  (jesus [at] blindsidenetworks [dt] com)    
  *
  * @package   mod_recordingsbn
- * @copyright 2011-2012 Blindside Networks Inc.
+ * @copyright 2011-2013 Blindside Networks Inc.
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2013013000;
+$module->version   = 2013030100;
 $module->requires  = 2010112400;
 $module->cron      = 0;
 $module->component = 'mod_recordingsbn';
 $module->maturity = MATURITY_BETA;  // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
 $module->release  = '1.0.9';
 $module->dependencies = array(
-    'mod_bigbluebuttonbn' => 2013013000,
+    'mod_bigbluebuttonbn' => 2013030100,
 );
