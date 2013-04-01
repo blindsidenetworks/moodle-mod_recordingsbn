404997b53ef5028b1ec1151cd55ea6d031852a9e
jfederico <jesus@123it.ca>
2013-04-01 12:59:09 -0400

    v1.0.9b3: Updated version tag

--- version.php
+++ version.php
@@ -12,12 +12,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2013030102;
+$module->version   = 2013040100;
 $module->requires  = 2010112400;
 $module->cron      = 0;
 $module->component = 'mod_recordingsbn';
 $module->maturity = MATURITY_BETA;  // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
 $module->release  = '1.0.9';
 $module->dependencies = array(
-    'mod_bigbluebuttonbn' => 2013030100,
+    'mod_bigbluebuttonbn' => 2013040100,
 );
