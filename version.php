74f10bb87627a2742703d0d55642f7b6d7874ff0
Jesus Federico <jesus@123it.ca>
2013-05-02 14:43:57 -0400

    1.0.9-rc1: Updated tag to rc1 as part of the monthly release

--- version.php
+++ version.php
@@ -12,12 +12,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2013040105;
+$module->version   = 2013050100;
 $module->requires  = 2010112400;
 $module->cron      = 0;
 $module->component = 'mod_recordingsbn';
-$module->maturity = MATURITY_BETA;  // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
+$module->maturity = MATURITY_RC;  // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
 $module->release  = '1.0.9';
 $module->dependencies = array(
-    'mod_bigbluebuttonbn' => 2013040100,
+    'mod_bigbluebuttonbn' => 2013050100,
 );
