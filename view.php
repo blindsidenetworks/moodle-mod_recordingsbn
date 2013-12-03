2a61b73bc0066034583a6b145e0f759085c72f19
Jesus Federico <jesus@123it.ca>
2013-12-03 13:38:43 -0500

    Fixed a pretty old bug in dates.

--- view.php
+++ view.php
@@ -213,11 +213,13 @@ if ($dbman->table_exists('bigbluebuttonbn_log') ) {
                     	$recording['startTime'] = $recording['startTime'] / 1000;
                     }
                     //Set corresponding format
-                    //$format = isset(get_string('strftimerecentfull', 'langconfig'));
-                    //if( !isset($format) )
-                    $format = '%a %h %d, %Y %H:%M:%S %Z';
-                    //Format the date
-                    $formatedStartDate = userdate($recording['startTime'], $format, usertimezone($USER->timezone) );
+                    $format = get_string('strftimerecentfull', 'langconfig');
+                    if( isset($format) ) {
+                        $formatedStartDate = userdate($recording['startTime'], $format);
+                    } else {
+                        $format = '%a %h %d, %Y %H:%M:%S %Z';
+                        $formatedStartDate = userdate($recording['startTime'], $format, usertimezone($USER->timezone) );
+                    }
 
                     if ( $moderator ) {
                         $table->data[] = array ($type, $meta_activity, $meta_description, str_replace(" ", "&nbsp;", $formatedStartDate), $duration, $actionbar );
