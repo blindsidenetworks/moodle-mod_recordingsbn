3cd507de8593f713201520a313bdc2792762bd31
Jesus Federico <jesus@123it.ca>
2014-04-03 13:52:28 -0400

    Updated version tag

--- version.php
+++ version.php
@@ -12,7 +12,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2013110103;
+$module->version   = 2013110104;
 $module->requires  = 2010112400;
 $module->cron      = 0;
 $module->component = 'mod_recordingsbn';
