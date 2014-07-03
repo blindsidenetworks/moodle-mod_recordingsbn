1e492379d6e354f2049f4b7e9ac35b90b02be3e5
Jesus Federico <jesus@123it.ca>
2014-07-03 13:17:42 -0400

    v1.0.10-rc1: Updated required moodle version

--- version.php
+++ version.php
@@ -13,7 +13,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2014070100;
-$plugin->requires  = 2010112400;
+$plugin->requires  = 2013101800;
 $plugin->cron      = 0;
 $plugin->component = 'moodle-mod_recordingsbn';
 $plugin->maturity = MATURITY_RC;  // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
