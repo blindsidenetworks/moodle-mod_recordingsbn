42e5cc9433f7dfab385d02135ae9917620ea30f4
Jesus Federico <jesus@123it.ca>
2015-06-26 12:20:21 -0400

    Updated version tag in preparation for a new release

--- version.php
+++ version.php
@@ -10,18 +10,18 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-#$module->version = 2014101000;
+#$module->version = 2015062100;
 #$module->requires = 2010112400;
 #$module->cron = 0;
 #$module->component = 'mod_recordingsbn';
 #$module->maturity = MATURITY_STABLE;    // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
-#$module->release = '1.0.11';
+#$module->release = '1.1.0';
 #$module->dependencies = array( 'mod_bigbluebuttonbn' => 2014070310 );
 
-$plugin->version = 2014101000;
+$plugin->version = 2015062100;
 $plugin->requires = 2013111800;
 $plugin->cron = 0;
 $plugin->component = 'mod_recordingsbn';
 $plugin->maturity = MATURITY_STABLE;    // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
-$plugin->release = '1.0.11';
+$plugin->release = '1.1.0';
 $plugin->dependencies = array( 'mod_bigbluebuttonbn' => 2014070400 );
