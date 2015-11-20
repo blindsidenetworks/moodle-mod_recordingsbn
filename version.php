3c483f1949ed4a489ec5668bf33c4e471de67987
jfederico <jesus@123it.ca>
2015-11-20 12:19:49 -0500

    v1.2.0: Updated version tag in preparation for release

--- version.php
+++ version.php
@@ -10,10 +10,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015080604;
+$plugin->version = 2015080606;
 $plugin->requires = 2013111800;
 $plugin->cron = 0;
 $plugin->component = 'mod_recordingsbn';
-$plugin->maturity = MATURITY_RC;    // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
+$plugin->maturity = MATURITY_STABLE;    // [MATURITY_STABLE | MATURITY_RC | MATURITY_BETA | MATURITY_ALPHA]
 $plugin->release = '1.2.0';
 $plugin->dependencies = array( 'mod_bigbluebuttonbn' => 2015080600 );
