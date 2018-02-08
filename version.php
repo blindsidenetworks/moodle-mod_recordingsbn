aaaefed178ac71fcfb251a446095465c19700cb6
jfederico <jesus@123it.ca>
2018-02-08 16:52:26 +0000

    v1.3.3: Bumped up version tag

--- version.php
+++ version.php
@@ -19,16 +19,16 @@
  *
  * @package   mod_recordingsbn
  * @author    Jesus Federico  (jesus [at] blindsidenetworks [dt] com)
- * @copyright 2011-2015 Blindside Networks Inc.
+ * @copyright 2011-2018 Blindside Networks Inc.
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016051906;
+$plugin->version = 2016051907;
 $plugin->requires = 2013111800;
 $plugin->cron = 0;
 $plugin->component = 'mod_recordingsbn';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.3.2';
+$plugin->release = '1.3.3';
 $plugin->dependencies = array( 'mod_bigbluebuttonbn' => 2016051900 );
